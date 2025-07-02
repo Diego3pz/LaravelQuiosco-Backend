c325be0caaec05c51621b83bdaff4fe58814b11d
Diego de Jesús Espinoza García <di3goDjesus@hotmail.com>
2025-07-01 18:20:01 -0700

    feat(auth): agregar validaciones personalizadas para registro y login de usuario

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password as PasswordRules;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => [
+                'required',
+                'confirmed',
+                PasswordRules::min(8)
+                    ->letters()
+                    ->numbers()
+                    ->symbols()
+            ]
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'El nombre es obligatorio.',
+            'email.required' => 'El correo electrónico es obligatorio.',
+            'email.email' => 'El correo electrónico debe ser una dirección de correo electrónico válida.',
+            'email.unique' => 'El correo electrónico ya ha sido registrado.',
+            'password' => 'La contraseña debe tener al menos 8 caracteres, un símbolo y un número.',
+        ];
+    }
+}
