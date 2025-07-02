b804fa355df9109cd083b38fac981a375f5f0e83
Diego de Jesús Espinoza García <di3goDjesus@hotmail.com>
2025-07-02 16:57:37 -0700

    refactor(api): actualizar rutas y controlador de autenticación para soporte de órdenes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,3 +17,7 @@ Route::apiResource('/products', ProductController::class);
 // Autenticacion
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+// Almacenar ordenes
+Route::apiResource('/orders', OrderController::class)->middleware('auth:sanctum');
