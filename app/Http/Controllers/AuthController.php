3fa3a81c13dbc2efe4f57fc21dba0ba4beba46a0
Diego de Jesús Espinoza García <di3goDjesus@hotmail.com>
2025-07-01 18:18:44 -0700

    feat(auth): implementar registro y login de usuario con tokens

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        // Validad registro
+        $data = $request->validated();
+
+        // Crear usuario
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        // Retornar respuesta
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user,
+        ];
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $data = $request->validated();
+
+        // validar password
+        if (!Auth::attempt($data)) {
+            return response(
+                ['errors' => ['El email o la contraseña son incorrectos']],
+                422
+            );
+        }
+
+        // Autenticar usuario
+
+        $user = Auth::user();
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user,
+        ];
+    }
+
+    public function logout(Request $request) {}
+}
